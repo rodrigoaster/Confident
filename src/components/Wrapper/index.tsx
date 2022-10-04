799207ea855180e97a62dff917e79ae83cfc06e4
Rodrigo Aster <rodrigowebx@gmail.com>
2022-10-03 21:55:24 -0300

    Implementação do Provider na div Wrapper

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,9 +1,42 @@
-import { Container } from './styles';
+import { useCallback, useRef, useState } from 'react';
+import NewsContext, { NewsData } from '../Context/NewsContext';
+import { Container, OverlayRoot } from './styles';
+import { NewsOverlay } from '../NewsOverlay';
 
 export function Wrapper({ children }) {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const [registeredNews, setRegisteredNews] = useState<NewsData[]>([])
+
+  const registerNews = useCallback((news: NewsData) => {
+    setRegisteredNews(state => [...state, news])
+  }, [])
+
+  const unregisterNews = useCallback((sectionName: string) => {
+    setRegisteredNews(state => state.filter(news => news.sectionName !== sectionName))
+  }, [])
+
+  const getNewsByName = useCallback((sectionName: string) => {
+    return registeredNews.find(item => item.sectionName === sectionName) || null
+  }, [])
+  
   return (
-    <Container>
-      {children}
-    </Container>
+    <NewsContext.Provider value={{
+      wrapperRef,
+      registeredNews,
+      registerNews,
+      unregisterNews,
+      getNewsByName
+    }}>
+      <Container ref={wrapperRef}>
+        <OverlayRoot>
+          {registeredNews.map(item => (
+            <NewsOverlay key={item.sectionName}>{item.overlayDefault}</NewsOverlay>
+          ))}
+        </OverlayRoot>
+
+        {children}
+      </Container>
+    </NewsContext.Provider>
   );
 }
