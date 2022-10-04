da99c734e2e0077a1822e3bf51806c48ab53f8ac
Rodrigo Aster <rodrigowebx@gmail.com>
2022-10-03 21:57:09 -0300

    Criação dos OverlaysDefaults nos layouts

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
+import useNews from '../../hooks/useNews';
 import { Container } from './styles';
 
 interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -11,8 +12,18 @@ export function Section({
   overlayDefault, 
   children, 
   ...props }: SectionProps) {
+    const { registerNews } = useNews(sectionName);
+
+    const sectionRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+      if (sectionRef.current) {
+        registerNews({ sectionName, overlayDefault, sectionRef })
+      }
+    }, [])
+
   return (
-    <Container {...props}>
+    <Container ref={sectionRef} {...props}>
       {children}
     </Container>
   );
